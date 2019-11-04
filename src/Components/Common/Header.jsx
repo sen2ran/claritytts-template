2b139e7fda56fe1322f04d9cfb310444cd795bb3
Senthuran Elangovan <senthuran@pikanite.com>
2019-11-04 16:43:00 +0530

    Dropdown Change

--- src/Components/Common/Header.jsx
+++ src/Components/Common/Header.jsx
@@ -5,21 +5,16 @@ import { changeLangFn } from "../../store/actions/index";
 import { Link, withRouter } from "react-router-dom";
 
 class Header extends Component {
-  countryChangeFn(e) {
-    const { changeLangFn, history, location } = this.props;
-    changeLangFn(e.target.value);
-    history.push("/" + e.target.value + "/" + location.pathname.split("/")[2]);
-  }
+  // countryChangeFn(e) {
+  //   const { changeLangFn, history, location } = this.props;
+  //   changeLangFn(e.target.value);
+  //   history.push("/" + e.target.value + "/" + location.pathname.split("/")[2]);
+  // }
   render() {
     const { navDetails, country } = this.props.header;
     return (
       <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
         <h5 className="my-0 mr-md-auto font-weight-normal">Header</h5>
-
-        <select value={country} onChange={e => this.countryChangeFn(e)}>
-          <option value="en">En</option>
-          <option value="ru">Ru</option>
-        </select>
         <nav className="my-2 my-md-0 mr-md-3">
           {navDetails.map(navDetail => (
             <Link
