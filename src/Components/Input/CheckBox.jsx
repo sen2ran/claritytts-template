3613c8bb79ab339aa43a5b9b2a17ab998895ac0a
Senthuran Elangovan <senthuran@pikanite.com>
2019-11-04 15:18:08 +0530

    CheckBox Added

---
+++ src/Components/Input/CheckBox.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CheckBox = ({ label, isSelected, onCheckboxChange }) => (
+  <div className="col-4">
+    <div className="form-check">
+      <label>
+        <input
+          type="checkbox"
+          name={label}
+          checked={isSelected}
+          onChange={onCheckboxChange}
+          className="form-check-input"
+        />
+        {label}
+      </label>
+    </div>
+  </div>
+);
+
+export default CheckBox;
